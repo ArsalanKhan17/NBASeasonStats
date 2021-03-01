78c8090a89bdf1896bac05a53fee2793e43df165
ArsalanKhan17 <60335196+ArsalanKhan17@users.noreply.github.com>
2021-03-01 16:20:47 -0500

    Fixed error in loading data

--- R/Season_Stats_NBA.R
+++ R/Season_Stats_NBA.R
@@ -1,3 +1,3 @@
-Season_Stats_NBA  <- readr::read_csv("G:/My Drive/Spring/Modern Data Structures/HW5/Seasons_Stats_NBA.csv")
-
-usethis::use_data(Season_Stats_NBA, overwrite = TRUE)
+# Season_Stats_NBA  <- readr::read_csv("G:/My Drive/Spring/Modern Data Structures/HW5/Seasons_Stats_NBA.csv")
+#
+# usethis::use_data(Season_Stats_NBA, overwrite = TRUE)
